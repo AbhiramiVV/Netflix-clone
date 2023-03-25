18c6211e1e3af660d51e03bd0b87a4724ffa6460
AbhiramiVV <abhiramivv77@gmail.com>
2023-03-25 10:01:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import { action,originals,Comedy,Horror,Romance,trending } from "./urls";
 import './App.css';
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <NavBar/>
+        <Banner/>
+        <RowPost url={originals} title='Netflix Orginals'/>
+        <RowPost url={trending} title='Trending Movies' isSmall/>
+        <RowPost url={action} title='Action Movies' isSmall/>
+        <RowPost url={Comedy} title='Comedy Movies' isSmall/>
+        <RowPost url={Horror} title='Horror Movies' isSmall/>
+        <RowPost url={Romance} title='Romance Movies' isSmall/>
+       
     </div>
   );
 }
